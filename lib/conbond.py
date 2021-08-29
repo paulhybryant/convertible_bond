7b2d172aa4400716a403412c2a85cdfaeb7795a0
paulhybryant <paulhybryant@gmail.com>
2021-08-30 07:45:39 +0800

    jisilu

--- lib/conbond.py
+++ lib/conbond.py
@@ -1,8 +1,9 @@
 import numpy as np
 import os
 import pandas as pd
-from datetime import date, timedelta
-from collections.abc import Callable
+import requests
+from datetime import datetime, date, timedelta
+import json
 
 
 # To use this locally, need to call auth() first
@@ -29,11 +30,13 @@ def fetch_jqdata(jqdata, today):
     return txn_day, df_basic_info, df_latest_bond_price, df_latest_stock_price, df_convert_price_adjust
 
 
-def massage_data(df_basic_info, df_latest_bond_price, df_latest_stock_price,
-                 df_convert_price_adjust):
+def massage_jqdata(df_basic_info, df_latest_bond_price, df_latest_stock_price,
+                   df_convert_price_adjust):
     # Data cleaning
     df_basic_info = df_basic_info[[
-        'code', 'short_name', 'company_code', 'convert_price'
+        'code',
+        'short_name',
+        'company_code',
     ]]
     df_latest_bond_price = df_latest_bond_price[[
         'code', 'exchange_code', 'close'
@@ -44,9 +47,11 @@ def massage_data(df_basic_info, df_latest_bond_price, df_latest_stock_price,
     df_convert_price_adjust = df_convert_price_adjust[[
         'code', 'new_convert_price'
     ]].groupby('code').min()
+    df_convert_price_adjust = df_convert_price_adjust.rename(
+        columns={'new_convert_price': 'convert_price'})
 
     # Join basic_info with latest_bond_price to get close price from last transaction day
-    # Schema: code, short_name, company_code, convert_price, bond_price
+    # Schema: code, short_name, company_code, bond_price
     df = df_basic_info.set_index('code').join(
         df_latest_bond_price.set_index('code')).reset_index()
     # Keep only bonds that are listed and also can be traded
@@ -56,35 +61,40 @@ def massage_data(df_basic_info, df_latest_bond_price, df_latest_stock_price,
     # Join with convert_price_adjust to get latest convert price
     # code in convert_price_latest is str, while code in df is int64
     df['code'] = df.code.astype(str)
-    # Schema: code, short_name, company_code, convert_price, bond_price, new_convert_price
+    # Schema: code, short_name, company_code, bond_price, convert_price
     df = df.set_index('code').join(df_convert_price_adjust)
 
     # Join with latest_stock_price to get latest stock price
-    # Schema: code, short_name, company_code, convert_price, bond_price, new_convert_price, stock_price
+    # Schema: code, short_name, company_code, bond_price, convert_price, stock_price
     df = df.reset_index().set_index('company_code').join(
         df_latest_stock_price.set_index('code'))
 
     # Calculate convert_premium_rate
-    # Schema: code, short_name, company_code, convert_price, bond_price, new_convert_price, stock_price, convert_premium_rate
-    df['convert_premium_rate'] = df.bond_price / (100 / df.new_convert_price *
+    # Schema: code, short_name, company_code, bond_price, convert_price, stock_price, convert_premium_rate
+    df['convert_premium_rate'] = df.bond_price / (100 / df.convert_price *
                                                   df.stock_price) - 1
     return df
 
 
 # config: Expect to have two keys: weight_bond_price and weight_convert_premium_rate
+# df: Expect to have a column named 'double_low', or two columns named 'bond_price' and 'convert_premium_rate'
 def double_low(df, config):
-    dl_df = df
-    if not 'weight_bond_price' in config:
-        raise 'Bad config: weight_bond_price not found'
-    if not 'weight_convert_premium_rate' in config:
-        raise 'Bad config: weight_convert_premium_rate not found'
-    if not 'top' in config:
-        raise 'Bad config: top not found'
-    weight_bond_price = config['weight_bond_price']
-    weight_convert_premium_rate = config['weight_convert_premium_rate']
+    assert 'top' in config
     top = config['top']
-    dl_df[
-        'double_low'] = df.bond_price * weight_bond_price + df.convert_premium_rate * 100 * weight_convert_premium_rate
+
+    dl_df = None
+    if 'double_low' in df.columns:
+        dl_df = df
+    else:
+        dl_df = df
+        assert 'weight_bond_price' in config
+        assert 'weight_convert_premium_rate' in config
+        weight_bond_price = config['weight_bond_price']
+        weight_convert_premium_rate = config['weight_convert_premium_rate']
+        assert 'bond_price' in dl_df.columns
+        assert 'convert_premium_rate' in dl_df.columns
+        dl_df[
+            'double_low'] = dl_df.bond_price * weight_bond_price + dl_df.convert_premium_rate * 100 * weight_convert_premium_rate
     return dl_df.nsmallest(top, 'double_low')
 
 
@@ -96,17 +106,47 @@ def generate_orders(holdings, candidates):
     return orders
 
 
-def execute_strategy(df, strategy, config):
-    return strategy(df, config)
-
-
-def generate_candidates(df_basic_info, df_latest_bond_price,
-                        df_latest_stock_price, df_convert_price_adjust,
-                        strategy, strategy_config, holdings):
-    df = massage_data(df_basic_info, df_latest_bond_price,
-                      df_latest_stock_price, df_convert_price_adjust)
-    candidates = execute_strategy(df, strategy, strategy_config)
+def generate_candidates(df, strategy, strategy_config, holdings):
+    candidates = strategy(df, strategy_config)
     candidates['code'] = candidates[['code', 'exchange_code']].agg('.'.join,
                                                                    axis=1)
     orders = generate_orders(holdings, set(candidates.code.tolist()))
     return candidates, orders
+
+
+def fetch_jisilu():
+    today = date.today()
+    url = 'https://www.jisilu.cn/data/cbnew/cb_list/?___jsl=LST___t=%s' % int(
+        datetime.fromordinal(today.toordinal()).timestamp() * 1000)
+    payload = {'listed': 'Y'}
+    response = requests.post(url, data=payload)
+    # 当爬取的界面需要用户名密码登录时候，构建的请求需要包含auth字段
+    data = response.content.decode('utf-8')
+    return today - timedelta(days=1), data
+
+
+def massage_jisiludata(data):
+    jd = {}
+    for row in json.loads(data)['rows']:
+        jd[row['id']] = row['cell']
+    df = pd.io.json.read_json(json.dumps(jd), orient='index')
+    # filter exchangeable bond
+    df = df[df['btype'] == 'C']
+    df = df.rename(
+        columns={
+            'bond_id': 'code',
+            'bond_nm': 'short_name',
+            'stock_id': 'company_code',
+            'price': 'bond_price',
+            'sprice': 'stock_price',
+            'premium_rt': 'convert_premium_rate',
+            'dblow': 'double_low'
+        }).reset_index()
+    df['exchange_code'] = df.reset_index().apply(
+        lambda row: 'XSHE' if row.company_code.startswith('sz') else 'XSHG',
+        axis=1)
+    df['code'] = df['code'].astype(str)
+    return df[[
+        'code', 'short_name', 'company_code', 'exchange_code', 'bond_price',
+        'stock_price', 'convert_premium_rate', 'double_low'
+    ]]
--- main.py
+++ main.py
@@ -2,58 +2,61 @@
 
 import os
 import json
+import pandas as pd
 import pprint
 from absl import app, flags, logging
-from datetime import date
+from datetime import date, timedelta
 
 import jqdatasdk as jqdata
 from lib import conbond
 
 FLAGS = flags.FLAGS
 
-flags.DEFINE_bool("use_cache", False, "Use cached data")
+flags.DEFINE_bool("use_cache", False, "Use cache or not")
 flags.DEFINE_string("cache_dir", None, "Cache directory")
 flags.DEFINE_integer("top", 20, "Number of candidates")
-flags.DEFINE_string("data_source", "jqdata", "Data source: JQData, jisilu")
+flags.DEFINE_string("data_source", "jqdata",
+                    "Data source: jqdata, cache, jisilu")
 
 
 def main(argv):
-    df_basic_info = None  # Basic information of the bond
-    df_latest_bond_price = None  # Latest price of the bond
-    df_latest_stock_price = None  # Latest price of the stock
-    df_convert_price_adjust = None  # Latest convert price of the bond
     df_date = None  # Date of the price information
+    df = None
 
-    if FLAGS.use_cache:
-        df_date, df_basic_info, df_latest_bond_price, df_latest_stock_price, df_convert_price_adjust = fetch_cache(
-            FLAGS.cache_dir)
-        logging.info('Using cached data from date: %s' %
-                     df_date.strftime('%Y-%m-%d'))
-    else:
-        if FLAGS.data_source == 'jqdata':
+    if FLAGS.data_source == 'jqdata':
+        df_basic_info = None  # Basic information of the bond
+        df_latest_bond_price = None  # Latest price of the bond
+        df_latest_stock_price = None  # Latest price of the stock
+        df_convert_price_adjust = None  # Latest convert price of the bond
+        if not FLAGS.use_cache:
             jqconfig = json.load(open('jqconfig.json'))
             jqdata.auth(jqconfig['username'], jqconfig['password'])
             df_date, df_basic_info, df_latest_bond_price, df_latest_stock_price, df_convert_price_adjust = conbond.fetch_jqdata(
                 jqdata, date.today())
-            if FLAGS.cache_dir:
-                df_basic_info.to_excel(
-                    os.path.join(FLAGS.cache_dir, 'basic_info.xlsx'))
-                df_latest_bond_price.to_excel(
-                    os.path.join(FLAGS.cache_dir, 'latest_bond_price.xlsx'))
-                df_latest_stock_price.to_excel(
-                    os.path.join(FLAGS.cache_dir, 'latest_stock_price.xlsx'))
-                df_convert_price_adjust.to_excel(
-                    os.path.join(FLAGS.cache_dir, 'convert_price_adjust.xlsx'))
-            logging.info('Using latest jqdata from date: %s' %
-                         df_date.strftime('%Y-%m-%d'))
-        else:
-            # TODO: get data from jisilu
-            pass
+            logging.info('Fetch latest jqdata')
+            cache_jqdata(FLAGS.cache_dir, df_basic_info, df_latest_bond_price,
+                         df_latest_stock_price, df_convert_price_adjust)
+        df_date, df_basic_info, df_latest_bond_price, df_latest_stock_price, df_convert_price_adjust = fetch_jqcache(
+            FLAGS.cache_dir)
+        logging.info('Using data from date: %s' % df_date.strftime('%Y-%m-%d'))
+        df = conbond.massage_jqdata(df_basic_info, df_latest_bond_price,
+                                    df_latest_stock_price,
+                                    df_convert_price_adjust)
+    elif FLAGS.data_source == 'jisilu':
+        if not FLAGS.use_cache:
+            df_date, data = conbond.fetch_jisilu()
+            logging.info('Fetching latest jisilu data')
+            cache_jisilu(FLAGS.cache_dir, data)
+        df_date, data = fetch_jisilucache(FLAGS.cache_dir)
+        logging.info('Using jisilu data from date: %s' %
+                     df_date.strftime('%Y-%m-%d'))
+        df = conbond.massage_jisiludata(data)
+    else:
+        raise
 
     logging.info('Using double_low strategy')
     candidates, orders = conbond.generate_candidates(
-        df_basic_info, df_latest_bond_price, df_latest_stock_price,
-        df_convert_price_adjust, conbond.double_low, {
+        df, conbond.double_low, {
             'weight_bond_price': 0.5,
             'weight_convert_premium_rate': 0.5,
             'top': FLAGS.top,
@@ -65,7 +68,7 @@ def main(argv):
     pprint.pprint(orders)
 
 
-def fetch_cache(cache_dir):
+def fetch_jqcache(cache_dir):
     df_basic_info = pd.read_excel(os.path.join(cache_dir, 'basic_info.xlsx'))
     df_latest_bond_price = pd.read_excel(
         os.path.join(cache_dir, 'latest_bond_price.xlsx'))
@@ -75,6 +78,28 @@ def fetch_cache(cache_dir):
         os.path.join(cache_dir, 'convert_price_adjust.xlsx'))
     return df_latest_bond_price.date[
         0], df_basic_info, df_latest_bond_price, df_latest_stock_price, df_convert_price_adjust
+
+
+def cache_jqdata(cache_dir, df_basic_info, df_latest_bond_price,
+                 df_latest_stock_price, df_convert_price_adjust):
+    df_basic_info.to_excel(os.path.join(cache_dir, 'basic_info.xlsx'))
+    df_latest_bond_price.to_excel(
+        os.path.join(cache_dir, 'latest_bond_price.xlsx'))
+    df_latest_stock_price.to_excel(
+        os.path.join(cache_dir, 'latest_stock_price.xlsx'))
+    df_convert_price_adjust.to_excel(
+        os.path.join(cache_dir, 'convert_price_adjust.xlsx'))
+
+
+def fetch_jisilucache(cache_dir):
+    cache = open(os.path.join(cache_dir, 'jisilu.json'), 'r', encoding='utf-8')
+    return date.today() - timedelta(days=1), cache.read()
+
+
+def cache_jisilu(cache_dir, data):
+    cache = open(os.path.join(cache_dir, 'jisilu.json'), 'w', encoding='utf-8')
+    cache.write(data)
+    cache.close()
 
 
 if __name__ == "__main__":
