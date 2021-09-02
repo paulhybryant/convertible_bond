28420d3d9d5fa04c168fbc5bc430d554b07025e8
paulhybryant <paulhybryant@gmail.com>
2021-09-02 16:54:14 +0800

    .

--- lib/conbond.py
+++ lib/conbond.py
@@ -114,38 +114,6 @@ def fetch_jqdata(username, password, jqdata, today, cache_dir, use_cache):
     return txn_day, df
 
 
-# config: Expect to have two keys: weight_bond_price and weight_convert_premium_rate
-# df: Expect to have a column named 'double_low', or two columns named 'bond_price' and 'convert_premium_rate'
-def double_low(df, config):
-    assert 'top' in config
-    top = config['top']
-
-    dl_df = None
-    if 'double_low' in df.columns:
-        dl_df = df
-    else:
-        dl_df = df
-        assert 'weight_bond_price' in config
-        assert 'weight_convert_premium_rate' in config
-        weight_bond_price = config['weight_bond_price']
-        weight_convert_premium_rate = config['weight_convert_premium_rate']
-        assert 'bond_price' in dl_df.columns
-        assert 'convert_premium_rate' in dl_df.columns
-        dl_df[
-            'double_low'] = dl_df.bond_price * weight_bond_price + dl_df.convert_premium_rate * 100 * weight_convert_premium_rate
-    return dl_df.nsmallest(top, 'double_low')
-
-
-def generate_candidates(df, strategy, strategy_config, holdings):
-    candidates = strategy(df, strategy_config)
-    candidate_codes = set(candidates.code.tolist())
-    orders = {}
-    orders['buy'] = list(candidate_codes - holdings)
-    orders['sell'] = list(holdings - candidate_codes)
-    orders['hold'] = list(holdings & candidate_codes)
-    return candidates, orders
-
-
 def fetch_jisilu(user_name, password, cache_dir, use_cache):
     jisilu_data = None
     txn_day = None
@@ -308,3 +276,35 @@ def fetch_rqdata(username, password, rqdatac, today, cache_dir, use_cache):
                                                   df.stock_price) - 1
     df = df.rename(columns={'order_book_id': 'code', 'symbol': 'short_name'})
     return txn_day, df
+
+
+# config: Expect to have two keys: weight_bond_price and weight_convert_premium_rate
+# df: Expect to have a column named 'double_low', or two columns named 'bond_price' and 'convert_premium_rate'
+def double_low(df, config):
+    assert 'top' in config
+    top = config['top']
+
+    dl_df = None
+    if 'double_low' in df.columns:
+        dl_df = df
+    else:
+        dl_df = df
+        assert 'weight_bond_price' in config
+        assert 'weight_convert_premium_rate' in config
+        weight_bond_price = config['weight_bond_price']
+        weight_convert_premium_rate = config['weight_convert_premium_rate']
+        assert 'bond_price' in dl_df.columns
+        assert 'convert_premium_rate' in dl_df.columns
+        dl_df[
+            'double_low'] = dl_df.bond_price * weight_bond_price + dl_df.convert_premium_rate * 100 * weight_convert_premium_rate
+    return dl_df.nsmallest(top, 'double_low')
+
+
+def generate_candidates(df, strategy, strategy_config, holdings):
+    candidates = strategy(df, strategy_config)
+    candidate_codes = set(candidates.code.tolist())
+    orders = {}
+    orders['buy'] = list(candidate_codes - holdings)
+    orders['sell'] = list(holdings - candidate_codes)
+    orders['hold'] = list(holdings & candidate_codes)
+    return candidates, orders
