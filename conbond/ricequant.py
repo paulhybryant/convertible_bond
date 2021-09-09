7845e82fb7bdbb38781040c9116975dbdfb9f2b9
paulhybryant <paulhybryant@gmail.com>
2021-09-09 12:24:03 +0800

    .

--- conbond/ricequant.py
+++ conbond/ricequant.py
@@ -5,36 +5,94 @@ import pandas as pd
 import pathlib
 
 
-def read_data(txn_day):
-    df_all_instruments = rqdatac.convertible.all_instruments(
-        txn_day).reset_index()
-    df_latest_bond_price = rqdatac.get_price(
-        df_all_instruments.order_book_id.tolist(),
-        start_date=txn_day,
-        end_date=txn_day,
-        frequency='1d').reset_index()
-    df_latest_stock_price = rqdatac.get_price(
-        df_all_instruments.stock_code.tolist(),
-        start_date=txn_day,
-        end_date=txn_day,
-        frequency='1d').reset_index()
-    df_conversion_price = rqdatac.convertible.get_conversion_price(
-        df_all_instruments.order_book_id.tolist(),
-        end_date=txn_day).reset_index()
-    df_call_info = rqdatac.convertible.get_call_info(
-        df_all_instruments.order_book_id.tolist(), end_date=txn_day)
-    if df_call_info is not None:
-        df_call_info = df_call_info.reset_index()
-    df_indicators = rqdatac.convertible.get_indicators(
-        df_all_instruments.order_book_id.tolist(),
-        start_date=txn_day,
-        end_date=txn_day).reset_index()
-    return df_all_instruments, df_conversion_price, df_latest_bond_price, df_latest_stock_price, df_call_info, df_indicators
+def read_or_none(cache_path, f):
+    p = cache_path.joinpath(f)
+    if p.exists():
+        return pd.read_excel(p)
+    return None
+
+
+def fetch(txn_day, cache_dir=None):
+    print(txn_day.strftime('%Y-%m-%d'))
+    df_all_instruments = None
+    df_conversion_price = None
+    df_latest_bond_price = None
+    df_latest_stock_price = None
+    df_call_info = None
+    df_indicators = None
+    df_suspended = None
+    cache_path = None
+
+    if cache_dir:
+        cache_path = pathlib.Path(cache_dir).joinpath(
+            'rqdata', txn_day.strftime('%Y-%m-%d'))
+        cache_path.mkdir(parents=True, exist_ok=True)
+        df_all_instruments = read_or_none(cache_path, 'all_instruments.xlsx')
+        df_conversion_price = read_or_none(cache_path, 'conversion_price.xlsx')
+        df_latest_bond_price = read_or_none(cache_path, 'bond_price.xlsx')
+        df_latest_stock_price = read_or_none(cache_path, 'stock_price.xlsx')
+        df_call_info = read_or_none(cache_path, 'call_info.xlsx')
+        df_indicators = read_or_none(cache_path, 'indicators.xlsx')
+        df_suspended = read_or_none(cache_path, 'suspended.xlsx')
+
+    if df_all_instruments is None:
+        df_all_instruments = rqdatac.convertible.all_instruments(
+            txn_day).reset_index()
+        if cache_path:
+            df_all_instruments.to_excel(
+                cache_path.joinpath('all_instruments.xlsx'))
+    if df_latest_bond_price is None:
+        df_latest_bond_price = rqdatac.get_price(
+            df_all_instruments.order_book_id.tolist(),
+            start_date=txn_day,
+            end_date=txn_day,
+            frequency='1d').reset_index()
+        if cache_path:
+            df_latest_bond_price.to_excel(
+                cache_path.joinpath('bond_price.xlsx'))
+    if df_latest_stock_price is None:
+        df_latest_stock_price = rqdatac.get_price(
+            df_all_instruments.stock_code.tolist(),
+            start_date=txn_day,
+            end_date=txn_day,
+            frequency='1d').reset_index()
+        if cache_path:
+            df_latest_stock_price.to_excel(
+                cache_path.joinpath('stock_price.xlsx'))
+    if df_conversion_price is None:
+        df_conversion_price = rqdatac.convertible.get_conversion_price(
+            df_all_instruments.order_book_id.tolist(),
+            end_date=txn_day).reset_index()
+        if cache_path:
+            df_conversion_price.to_excel(
+                cache_path.joinpath('conversion_price.xlsx'))
+    if df_call_info is None:
+        df_call_info = rqdatac.convertible.get_call_info(
+            df_all_instruments.order_book_id.tolist(), end_date=txn_day)
+        if df_call_info is not None:
+            df_call_info = df_call_info.reset_index()
+        if cache_path:
+            df_call_info.to_excel(cache_path.joinpath('call_info.xlsx'))
+    if df_indicators is None:
+        df_indicators = rqdatac.convertible.get_indicators(
+            df_all_instruments.order_book_id.tolist(),
+            start_date=txn_day,
+            end_date=txn_day).reset_index()
+        if cache_path:
+            df_indicators.to_excel(cache_path.joinpath('indicators.xlsx'))
+    if df_suspended is None:
+        df_suspended = rqdatac.convertible.is_suspended(
+            df_all_instruments.order_book_id.tolist(),
+            start_date=txn_day,
+            end_date=txn_day)
+        if cache_path:
+            df_suspended.to_excel(cache_path.joinpath('suspended.xlsx'))
+    return txn_day, df_all_instruments, df_conversion_price, df_latest_bond_price, df_latest_stock_price, df_call_info, df_indicators, df_suspended
 
 
 def process(txn_day, df_all_instruments, df_conversion_price,
             df_latest_bond_price, df_latest_stock_price, df_call_info,
-            df_indicators):
+            df_indicators, df_suspended):
     # Data cleaning
     # Filter non-conbond, e.g. exchange bond
     df_all_instruments = df_all_instruments[df_all_instruments.bond_type ==
@@ -88,54 +146,6 @@ def process(txn_day, df_all_instruments, df_conversion_price,
     df['convert_premium_rate'] = df.bond_price / (100 / df.conversion_price *
                                                   df.stock_price) - 1
     return df
-
-
-def fetch(txn_day, cache_dir=None, skip_process=False):
-    df_all_instruments = None
-    df_conversion_price = None
-    df_latest_bond_price = None
-    df_latest_stock_price = None
-    df_call_info = None
-    df_indicators = None
-    cache_path = None
-
-    if cache_dir:
-        cache_path = pathlib.Path(cache_dir).joinpath(
-            'rqdata', txn_day.strftime('%Y-%m-%d'))
-
-    if cache_path and cache_path.exists():
-        print('Using cached file: %s' % cache_path)
-        df_all_instruments = pd.read_excel(
-            cache_path.joinpath('all_instruments.xlsx'))
-        df_conversion_price = pd.read_excel(
-            cache_path.joinpath('conversion_price.xlsx'))
-        df_latest_bond_price = pd.read_excel(
-            cache_path.joinpath('bond_price.xlsx'))
-        df_latest_stock_price = pd.read_excel(
-            cache_path.joinpath('stock_price.xlsx'))
-        df_call_info = pd.read_excel(cache_path.joinpath('call_info.xlsx'))
-        df_indicators = pd.read_excel(cache_path.joinpath('indicators.xlsx'))
-    else:
-        df_all_instruments, df_conversion_price, df_latest_bond_price, df_latest_stock_price, df_call_info, df_indicators = read_data(
-            tnx_day)
-        print('Using data from: %s' % txn_day)
-        if cache_path:
-            cache_path.mkdir(parents=True, exist_ok=True)
-            df_all_instruments.to_excel(
-                cache_path.joinpath('all_instruments.xlsx'))
-            df_conversion_price.to_excel(
-                cache_path.joinpath('conversion_price.xlsx'))
-            df_latest_bond_price.to_excel(
-                cache_path.joinpath('bond_price.xlsx'))
-            df_latest_stock_price.to_excel(
-                cache_path.joinpath('stock_price.xlsx'))
-            df_call_info.to_excel(cache_path.joinpath('call_info.xlsx'))
-            df_indicators.to_excel(cache_path.joinpath('indicators.xlsx'))
-
-    if not skip_process:
-        return process(txn_day, df_all_instruments, df_conversion_price,
-                       df_latest_bond_price, df_latest_stock_price,
-                       df_call_info, df_indicators)
 
 
 def auth(username, password):
--- crawl_rq.py
+++ crawl_rq.py
@@ -3,15 +3,16 @@
 import json
 import pathlib
 from absl import app, flags, logging
-from datetime import date
+from datetime import date, timedelta
 from conbond import ricequant
 import pandas as pd
 
 FLAGS = flags.FLAGS
 
 flags.DEFINE_string("cache_dir", None, "Cache directory")
-flags.DEFINE_string("start_date", None, "Date to start")
-flags.DEFINE_string("end_date", None, "Date to end")
+flags.mark_flag_as_required('cache_dir')
+flags.DEFINE_string("start_date", date.today().strftime('%Y-%m-%d'), "Date to start")
+flags.DEFINE_string("end_date", date.today().strftime('%Y-%m-%d'), "Date to end")
 
 
 def main(argv):
@@ -24,19 +25,16 @@ def main(argv):
     password = auth['rqdata']['password']
 
     df_trading_dates = pd.read_excel('trading_dates.xlsx')
-    if FLAGS.start_date:
-        df_trading_dates = df_trading_dates[
-            df_trading_dates.trading_date.dt.date >= date.fromisoformat(
-                FLAGS.start_date)]
-    if FLAGS.end_date:
-        df_trading_dates = df_trading_dates[
-            df_trading_dates.trading_date.dt.date <= date.fromisoformat(
-                FLAGS.end_date)]
+    df_trading_dates = df_trading_dates[
+        df_trading_dates.trading_date.dt.date >= date.fromisoformat(
+            FLAGS.start_date)]
+    df_trading_dates = df_trading_dates[
+        df_trading_dates.trading_date.dt.date <= date.fromisoformat(
+            FLAGS.end_date)]
 
     ricequant.auth(username, password)
     for df_date in df_trading_dates.trading_date.tolist():
-        logging.info(df_date.strftime('%Y-%m-%d'))
-        ricequant.fetch(df_date, FLAGS.cache_dir, skip_process=True)
+        ricequant.fetch(df_date - timedelta(days=1), FLAGS.cache_dir)
 
 
 if __name__ == "__main__":
