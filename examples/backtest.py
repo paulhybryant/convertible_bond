05e2fd5a6401de7ae6b9e63827fe871bfd2322fb
paulhybryant <paulhybryant@gmail.com>
2021-10-28 10:27:29 +0800

    Multibacktest

--- examples/backtest.py
+++ examples/backtest.py
@@ -5,10 +5,11 @@ from conbond import ricequant, strategy
 from datetime import datetime
 from rqalpha import run_func
 from rqalpha.api import *
+import glob
 import json
 import logging
+import pandas as pd
 import pathlib
-import types
 
 FLAGS = flags.FLAGS
 
@@ -89,18 +90,111 @@ def rebalance(context, bar_dict):
             logging.info('Order error: %s' % order)
 
 
-def main(argv):
+def plot_results(results, savefile=None):
+    from matplotlib import rcParams, gridspec, ticker, image as mpimg, pyplot as plt
+    from matplotlib.font_manager import findfont, FontProperties
+    import numpy as np
+
+    rcParams['font.family'] = 'sans-serif'
+    rcParams['font.sans-serif'] = [
+        u'Microsoft Yahei',
+        u'Heiti SC',
+        u'Heiti TC',
+        u'STHeiti',
+        u'WenQuanYi Zen Hei',
+        u'WenQuanYi Micro Hei',
+        u"文泉驿微米黑",
+        u'SimHei',
+    ] + rcParams['font.sans-serif']
+    rcParams['axes.unicode_minus'] = False
+
+    use_chinese_fonts = True
+    font = findfont(FontProperties(family=['sans-serif']))
+    if "/matplotlib/" in font:
+        use_chinese_fonts = False
+        logging.warn("Missing Chinese fonts. Fallback to English.")
+
+    title = 'Conbond Strateties Comparison'
+    benchmark_portfolio = None
+    plt.style.use('ggplot')
+    plots_area_size = 0
+    img_width = 13
+    img_height = 6 + int(plots_area_size * 0.9)
+    fig = plt.figure(title, figsize=(img_width, img_height))
+    gs = gridspec.GridSpec(10 + plots_area_size, 8)
+    ax = plt.subplot(gs[2:10, :])
+    ax.get_xaxis().set_minor_locator(ticker.AutoMinorLocator())
+    ax.get_yaxis().set_minor_locator(ticker.AutoMinorLocator())
+    ax.grid(b=True, which='minor', linewidth=.2)
+    ax.grid(b=True, which='major', linewidth=1)
+    table_data = {}
+    table_columns = [
+        'sharpe', 'max_drawdown', 'total_returns', 'annualized_returns'
+    ]
+    for strategy, result_dict in results.items():
+        summary = result_dict["summary"]
+
+        if benchmark_portfolio is None:
+            benchmark_portfolio = result_dict.get("benchmark_portfolio")
+            index = benchmark_portfolio.index
+            portfolio_value = benchmark_portfolio.unit_net_value
+            xs = portfolio_value.values
+            rt = benchmark_portfolio.unit_net_value.values
+            max_dd_end = np.argmax(np.maximum.accumulate(xs) / xs)
+            if max_dd_end == 0:
+                max_dd_end = len(xs) - 1
+            max_dd_start = np.argmax(xs[:max_dd_end]) if max_dd_end > 0 else 0
+            max_dd = (
+                benchmark_portfolio.loc[index[max_dd_start]].unit_net_value -
+                benchmark_portfolio.loc[index[max_dd_end]].unit_net_value
+            ) / benchmark_portfolio.loc[index[max_dd_start]].unit_net_value
+            ax.plot(benchmark_portfolio["unit_net_value"] - 1.0,
+                    label="HS300",
+                    alpha=1,
+                    linewidth=2)
+            table_data['HS300'] = [
+                0, max_dd, summary['benchmark_total_returns'],
+                summary['benchmark_annualized_returns']
+            ]
+        table_data[strategy] = [summary[col] for col in table_columns]
+
+        portfolio = result_dict["portfolio"]
+        ax.plot(portfolio["unit_net_value"] - 1.0,
+                label=strategy,
+                alpha=1,
+                linewidth=2)
+
+    # place legend
+    leg = plt.legend(loc="best")
+    leg.get_frame().set_alpha(0.5)
+
+    # manipulate axis
+    vals = ax.get_yticks()
+    ax.set_yticklabels(['{:3.2f}%'.format(x * 100) for x in vals])
+
+    df = pd.DataFrame.from_dict(table_data,
+                                orient='index',
+                                columns=table_columns).reset_index().rename(
+                                    columns={'index': 'strategy'})
+    df[['max_drawdown', 'total_returns', 'annualized_returns'
+        ]] = df[['max_drawdown', 'total_returns',
+                 'annualized_returns']].applymap("{0:.2%}".format)
+    ax2 = plt.subplot(gs[0:2, :])
+    ax2.text(0, 1, 'Start Date: %s, End Date: %s' % (FLAGS.start_date, FLAGS.end_date))
+    ax2.table(cellText=df.values, colLabels=df.columns, loc='center')
+    ax2.axis('off')
+    #  ax2.axis('tight')
+
+    if savefile:
+        plt.savefig(savefile, bbox_inches='tight')
+
+
+def backtest(sc, run_dir, cache_dir):
+    p = pathlib.Path(sc)
+    logging.info(p.stem)
+    #  import types
     #  cfg = json.load(pathlib.Path(FLAGS.strategy_cfg).open(), object_hook=lambda d: types.SimpleNamespace(**d))
-    p = pathlib.Path(FLAGS.strategy_cfg)
     cfg = json.load(p.open())
-    run_dir = pathlib.Path('logs').joinpath(
-        p.stem,
-        datetime.now().strftime('%Y-%m-%d-%H-%M-%S'))
-    run_dir.mkdir(parents=True, exist_ok=False)
-    logging.basicConfig(level=logging.INFO,
-                        filename='%s/debug.log' % run_dir,
-                        filemode='w',
-                        force=True)
     config = {
         'base': {
             'start_date': FLAGS.start_date,
@@ -114,7 +208,7 @@ def main(argv):
         'extra': {
             'context_vars': {
                 'run_dir': run_dir.resolve(),
-                'cache_dir': FLAGS.cache_dir,
+                'cache_dir': cache_dir,
                 'strategy_name': cfg['name'],
                 'strategy_config': cfg['config'],
                 'top': cfg['top'],
@@ -125,9 +219,8 @@ def main(argv):
             'sys_analyser': {
                 'enabled': True,
                 'output_file': '%s/%s.pkl' % (run_dir, p.stem),
-                'report_save_path': '%s/report' % run_dir,
                 'plot': False,
-                'plot_save_file': '%s/plot.png' % run_dir,
+                'plot_save_file': '%s/%s.png' % (run_dir, p.stem),
             },
             'sys_simulation': {
                 'enabled': True,
@@ -167,16 +260,31 @@ def main(argv):
                 'end_date':
                 '2021-09-24',
                 'data_path':
-                pathlib.Path(__file__).parent.joinpath(FLAGS.cache_dir,
-                                                       'rqdata',
+                pathlib.Path(__file__).parent.joinpath(cache_dir, 'rqdata',
                                                        'combined.csv'),
                 'data_format':
                 'csv',
             }
         }
     }
-    run_func(init=init, config=config)
+    return p.stem, run_func(init=init, config=config)
+
+
+def main(argv):
+    run_dir = pathlib.Path('logs').joinpath(
+        datetime.now().strftime('%Y-%m-%d-%H-%M-%S'))
+    run_dir.mkdir(parents=True, exist_ok=False)
+    logging.basicConfig(level=logging.INFO,
+                        filename='%s/debug.log' % run_dir,
+                        filemode='w',
+                        force=True)
+
+    results = {}
+    for sc in glob.glob(FLAGS.strategy_cfg):
+        k, r = backtest(sc, run_dir, FLAGS.cache_dir)
+        results[k] = r['sys_analyser']
     print('Run dir: {0}, log: {0}/debug.log'.format(run_dir))
+    plot_results(results, savefile='results.png')
 
 
 if __name__ == '__main__':
